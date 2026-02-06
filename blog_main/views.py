ed86da1dc28aabefd554ceeba633ddad2e66af5d
Gaurav Totla <gauravmaheshwari59840@gmail.com>
2026-02-06 18:05:20 +0530

    blogs and search feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from blogs.models import Category,Blog
+from social_media.models import About
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False, status='Published')
-
+    try:
+        about = About.objects.get()
+    except:
+        about = "Hi, I'm Gaurav!"
     context = {
         'featured_posts' : featured_posts,
-        'posts' : posts
+        'posts' : posts,
+        'about' : about,
     }
     return render(request, 'home.html', context)
